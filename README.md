d73ba1ad5d70bec3445a1b597454e7ccce6ba433
MoriKen254 <masamorobo@gmail.com>
2016-12-20 22:38:40 +0900

    Update install script.

--- README.md
+++ README.md
@@ -42,17 +42,15 @@ $ git clone https://github.com/CIR-KIT-Unit03/cirkit_unit03_pkgs.git
 ```
 
 ##### 3. Run install script.
-- Without test
 ```bash
 $ cd <catkin_ws>/src/cirkit_unit03_pkgs
 $ sh install.sh
 ```
-
-- With test
-```bash
-$ cd <catkin_ws>/src/cirkit_unit03_pkgs
-$ sh install.sh -t
-```
+- optional arguments:
+  * -h: show this help message and exit"
+  * -i: eneble `rosdep init`"
+  * -t: enable `catkin_make run_tests`"
+  * -r: remove existing `src/.rosinstall`"
 
 ### From command line
 ##### 1. Create **catkinized**  workspace.
--- install.sh
+++ install.sh
@@ -1,29 +1,70 @@
 #!/bin/bash
 
+showHelp() {
+    echo 
+    echo "usege: install.sh [-h] [-i] [-t] [-r]"
+    echo 
+    echo "Clone all pacakges specifically related do CIR-KIT-Unit03."
+    echo "Packages to be cloned are defined in cirkit_unit03_pkgs.rosinstall."
+    echo 
+    echo "Basical packages for CIR-KIT-Unit03 are cloned into cirkit_unit03_pkgs directory."
+    echo "Dependent packages for CIR-KIT-Unit03 are cloned into cirkit_unit03_deps directory."
+    echo 
+    echo "Directory configuration after install is supposed to be as follows."
+    echo "  <catkin_ws>"
+    echo "  |"
+    echo "  |- src"
+    echo "     |"
+    echo "     |- cirkit_unit03_deps (Just a directory, not a metapacage)"
+    echo "     |   |"
+    echo "     |   |- (dependent pkgs cloned via cirkit_unit03_pkgs.rosinstall)"
+    echo "     |"
+    echo "     |- cirkit_unit03_pkgs"
+    echo "         |"
+    echo "         |- (basical pkgs cloned via cirkit_unit03_pkgs.rosinstall)"
+    echo 
+    echo "optional arguments:"
+    echo " -h           show this help message and exit"
+    echo " -i           eneble rosdep init"
+    echo " -t           enable catkin_make run_tests"
+    echo " -r           remove existing src/.rosinstall"
+}
+
+# Option -t enables test
+while getopts hirt OPT
+do
+  case $OPT in
+    "h" ) showHelp
+          exit 0 ;;
+    "i" ) FLG_I="TRUE" ;;
+    "r" ) FLG_R="TRUE" ;;
+    "t" ) FLG_T="TRUE" ;;
+      * ) echo "Usage: $CMDNAME [-h -i -t]" 1>&2
+          exit 1 ;;
+  esac
+done
+
 # Download required packages by wstool.
 cd ../../
+if [ "$FLG_R" = "TRUE" ]; then
+  echo "Removing src/.rosinstall"
+  rm src/.rosinstall
+fi
 wstool init src
 wstool merge -t src src/cirkit_unit03_pkgs/cirkit_unit03_pkgs.rosinstall
 wstool update -t src
 
 # Download depended packages by rosdep.
-#sudo rosdep init
-#rosdep update
+if [ "$FLG_I" = "TRUE" ]; then
+  sudo rosdep init
+fi
+rosdep update
 rosdep install -i -r -y --from-paths src --ignore-src
 
 # Build packages, and set the path for the packages.
 catkin_make
 source devel/setup.bash
 
-# Option -t enables test
-while getopts t OPT
-do
-  case $OPT in
-    "t" ) FLG_T="TRUE" ;;
-      * ) echo "Usage: $CMDNAME [-t]" 1>&2
-          exit 1 ;;
-  esac
-done
 
 if [ "$FLG_T" = "TRUE" ]; then
   catkin_make run_tests
